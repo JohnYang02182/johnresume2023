a4ed11e7aa3da0af81f4d8f3384fffbebdac677b
楊鈞皓 <john705810420@gmail.com>
2023-06-26 01:32:26 +0800

    [fix]: Erased the console.log.

--- util/globalSrc.ts
+++ util/globalSrc.ts
@@ -1,5 +1,4 @@
 export function GloSrc(imgPath:string){
   // const path = new URL(`/src/assets/images`,import.meta.url).href
-  // console.log(path)
   return new URL(`../assets/images/${imgPath}`, import.meta.url).href
 }
