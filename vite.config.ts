2e5038315b882f2769432c4238db88f2e4681ce1
楊鈞皓 <john705810420@gmail.com>
2023-07-05 13:57:43 +0800

    [fix]: Couldn't get font-family after build.

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     alias: [
       { find: '/@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
       { find: '/IMG', replacement: fileURLToPath(new URL('./src/assets/images/', import.meta.url)) },
+      { find: '/FONT', replacement: fileURLToPath(new URL('./src/assets/fonts/', import.meta.url)) }
     ],
     extensions: ['.js','.ts','.json','.jsx','.mjs','.tsx','.vue']
   },
