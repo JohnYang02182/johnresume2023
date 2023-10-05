a3e4903338389c4d122994c2842c3e8a3a9c018d
楊鈞皓 <john705810420@gmail.com>
2023-10-05 11:14:25 +0800

    [fix]: Fixed the haeder navbar have been covered by the body elements.

--- src/layouts/layoutBase.vue
+++ src/layouts/layoutBase.vue
@@ -10,6 +10,7 @@ import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
 </script>
 <style lang="scss" scoped>
+@import '../style/main_page.scss';
 :deep(){
   @import '../style/main_page.scss';
 }
