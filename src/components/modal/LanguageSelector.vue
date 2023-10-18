701970087147113b978e2ef4ac48f0a01f13571d
楊鈞皓 <john705810420@gmail.com>
2023-10-19 02:24:12 +0800

    [fix]: fixed some style issues of header.

--- src/components/modal/LanguageSelector.vue
+++ src/components/modal/LanguageSelector.vue
@@ -7,20 +7,22 @@
     </label>
     <ul class="selector-list" :class="{'active': listIsOpen, 'close': !listIsOpen}" @mouseenter="switchLanListOn()" @mouseleave="switchLanListOff()">
       <li v-for='(item, index) in SUPPORT_LOCALES' :key='index' class="selector-list-n-content">
-        <a class="list-items" href="javascript:void(0)" @click="switchLanguage(item.langOption)">{{ item.langName }}</a>
+        <a class="list-items" href="javascript:void(0)" @click="switchLanguage(item.langOption);switchLanListOff();">{{ item.langName }}</a>
       </li>
     </ul>
   </div>
 </template>
 <script lang='ts' setup>
-import { computed, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n'
 import { useUserStore } from '../../../store/user'
 import { SUPPORT_LOCALES } from '../../../util/i18n'
 const userStore = useUserStore()
 const listIsOpen = ref(false)
 const i18n = useI18n()
-
+onMounted(() => {
+  const windowWidth = ref(window.innerWidth)
+})
 const selectLanguage = computed({
   get: () => userStore.lang,
   set: (lang: string) => userStore.setLanguage(lang)
