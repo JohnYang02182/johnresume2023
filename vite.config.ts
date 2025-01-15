82767548de8e33d85b5abe6539d475115c236de4
楊鈞皓 <john705810420@gmail.com>
2025-01-16 01:34:58 +0800

    [fix]: Change the CJS building mode from Node API to Bundler.

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,24 @@
-import { fileURLToPath, URL } from 'url'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
 import vue from '@vitejs/plugin-vue'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { dirname, resolve } from 'node:path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 6060,
-  },
   plugins: [
     vue(),
     VueI18nPlugin({
-      include: [path.resolve(__dirname, './locales/**')],
-      strictMessage: false, // Add this line
-      escapeHtml: false // Add this line
-    }),
-    // pluginRewriteAll()
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './locales/**'),
+      strictMessage: false,
+      escapeHtml: false
+    })
   ],
   resolve: {
-    alias: [
-      { find: '/@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-      { find: '/IMG', replacement: fileURLToPath(new URL('./src/assets/images/', import.meta.url)) },
-      { find: '/FONT', replacement: fileURLToPath(new URL('./src/assets/fonts/', import.meta.url)) }
-    ],
-    extensions: ['.js','.ts','.json','.jsx','.mjs','.tsx','.vue']
+    alias: {
+      '/@': fileURLToPath(new URL('./src', import.meta.url)),
+      '/IMG': fileURLToPath(new URL('./src/assets/images', import.meta.url)),
+      '/FONT': fileURLToPath(new URL('./src/assets/fonts', import.meta.url))
+    }
   },
   base: '/'
 })
