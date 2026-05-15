d54285766e033a15d399e0b7403db99d57332357
john <johnyang@gmail.com>
2026-05-15 17:23:38 +0800

    [feat]: Break down the project detail page to components.

---
+++ src/components/Content.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="['content', isCol ? 'col' : 'row']">
+        <h2 class="main-title">{{ title }}</h2>
+        <div v-if="isList" class="list-wrapper dot-style">
+        <slot />
+        </div>
+        <div v-else class="content-text">
+        <slot />
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    isCol: {
+        type: Boolean,
+        default: false
+    },
+    isList: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
