cf6ae5571b79e8623562b29184f0536b7ed00d89
john <johnyang@gmail.com>
2026-06-04 16:49:47 +0800

    [fix]: Fixed the api handling method, remove the CSRF validation.

--- functions/auth.js
+++ functions/auth.js
@@ -1,18 +1,13 @@
-export async function onRequestGet({ request, env }) {
-    const { origin } = new URL(request.url)
-    const csrfToken = crypto.randomUUID().replaceAll('-', '')
-
+export async function onRequestGet({ env }) {
     const params = new URLSearchParams({
         client_id: env.GITHUB_CLIENT_ID,
         scope: 'repo,user',
-        state: csrfToken,
     })
 
     return new Response('', {
         status: 302,
         headers: {
             Location: `https://github.com/login/oauth/authorize?${params.toString()}`,
-            'Set-Cookie': `csrf-token=github_${csrfToken}; HttpOnly; Path=/; Max-Age=600; SameSite=Lax; Secure`,
         },
     })
 }
