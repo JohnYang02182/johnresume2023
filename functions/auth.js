794502771bf51038c967496c4c2ac96de860a0b1
john <johnyang@gmail.com>
2026-06-04 16:28:47 +0800

    [fix]: the cms auth couldn't work.

--- functions/auth.js
+++ functions/auth.js
@@ -1,4 +1,18 @@
-export async function onRequestGet({ env }) {
-  const url = `https://github.com/login/oauth/authorize?client_id=${env.GITHUB_CLIENT_ID}&scope=repo,user`
-  return Response.redirect(url, 302)
+export async function onRequestGet({ request, env }) {
+    const { origin } = new URL(request.url)
+    const csrfToken = crypto.randomUUID().replaceAll('-', '')
+
+    const params = new URLSearchParams({
+        client_id: env.GITHUB_CLIENT_ID,
+        scope: 'repo,user',
+        state: csrfToken,
+    })
+
+    return new Response('', {
+        status: 302,
+        headers: {
+            Location: `https://github.com/login/oauth/authorize?${params.toString()}`,
+            'Set-Cookie': `csrf-token=github_${csrfToken}; HttpOnly; Path=/; Max-Age=600; SameSite=Lax; Secure`,
+        },
+    })
 }
