11582d1fae30a13c8565d9492526bbf07c646d3b
楊鈞皓 <john705810420@gmail.com>
2026-07-06 01:36:55 +0800

    [del]: Remove the vitecomponet.

--- src/vitepressComponents/JSLearnMap.vue
+++ src/vitepressComponents/JSLearnMap.vue
@@ -56,56 +56,56 @@
 
 </script>
 <style scoped lang="scss">
-.container-wrapper {
-  position: relative;
-  box-sizing: border-box;
-  .card-wrapper {
-    position: relative;
-    padding: 16px;
-    width: 100%;
-    border-radius: 4px;
-    background-color: #1a1a1a;
-    .card-content {
-      display: flex;
-      gap: 16px;
-      flex-wrap: wrap;
-      align-content: center;
-      justify-self: center;
-      &-items {
-        flex: 1;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 24px 12px;
-        background-color: #42b883;
-        border-radius: 12px;
-        text-align: center;
-        font-size: 32px;
-        line-height: 1.2;
-      }
-      p {
-        margin: 0;
-        font-size: 32px;
-        line-height: 1.2;
-        text-align: center;
-      }
-      .card-content-footer {
-        display: block;
-        flex : 100%;
-        padding: 0 12px;
-        width: 100%;
-        height: 72px;
-        font-size: 32px;
-        line-height: 72px;
-      }
-    }
-  }
-  h3 {
-    margin: 0 0 12px 0;
-    font-size: 24px;
-    line-height: 1.5;
-    color: #ebebeb;
-    font-weight: bold;
-  }
-}
+// .container-wrapper {
+//   position: relative;
+//   box-sizing: border-box;
+//   .card-wrapper {
+//     position: relative;
+//     padding: 16px;
+//     width: 100%;
+//     border-radius: 4px;
+//     background-color: #1a1a1a;
+//     .card-content {
+//       display: flex;
+//       gap: 16px;
+//       flex-wrap: wrap;
+//       align-content: center;
+//       justify-self: center;
+//       &-items {
+//         flex: 1;
+//         display: flex;
+//         justify-content: center;
+//         align-items: center;
+//         padding: 24px 12px;
+//         background-color: #42b883;
+//         border-radius: 12px;
+//         text-align: center;
+//         font-size: 32px;
+//         line-height: 1.2;
+//       }
+//       p {
+//         margin: 0;
+//         font-size: 32px;
+//         line-height: 1.2;
+//         text-align: center;
+//       }
+//       .card-content-footer {
+//         display: block;
+//         flex : 100%;
+//         padding: 0 12px;
+//         width: 100%;
+//         height: 72px;
+//         font-size: 32px;
+//         line-height: 72px;
+//       }
+//     }
+//   }
+//   h3 {
+//     margin: 0 0 12px 0;
+//     font-size: 24px;
+//     line-height: 1.5;
+//     color: #ebebeb;
+//     font-weight: bold;
+//   }
+// }
 </style>
